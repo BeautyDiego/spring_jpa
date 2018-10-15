14476a93c67aff88d0ec21fbc45c62abd9d24f99
BeautyDiego <81896631@qq.com>
2018-10-15 14:28:00 +0800

    add base

--- src/main/java/com/bdmc/myjpa/services/IUserService.java
+++ src/main/java/com/bdmc/myjpa/services/IUserService.java
@@ -6,15 +6,7 @@ import com.bdmc.myjpa.entity.User;
 import org.springframework.data.domain.Example;
 
 
-public interface IUserService {
+public interface IUserService  extends IBaseService<User> {
  
-    public List<User> findAll();
-
-    public User findOne(Example<User> example);
-
-    public User findOneById(long id);
-
-    public User save(User user);
-
-    public void deleteById(long id);
+    
 }
